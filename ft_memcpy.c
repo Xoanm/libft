80a7651274ac255d98bdae37056163a11b85a2a3
juan M <juan@MacBook-Air-de-juan.local>
2022-06-26 18:18:50 +0200

    memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -10,4 +10,31 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+void *ft_memcpy(void	*s1, const	void *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s1 && !s2)
+		return (0);
+	while (i < n)
+	{
+		((unsigned char *)s1)[i] = ((unsigned char *)s2)[i];
+		i++;
+	}	
+	return (s1);
+}
+
+int	main(void)
+{
+	char	s1[] = "hola que tal";
+	char	s2[] = "Juan";
+
+	printf("%s\n", memcpy(s1, s2, 4));
+	printf("%s\n", ft_memcpy(s1, s2, 4));
+	return (0);
+}
