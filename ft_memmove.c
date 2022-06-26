3e2b242c5b14252684390b7bb8d461d0b5f54a4f
juan M <juan@MacBook-Air-de-juan.local>
2022-06-27 00:01:56 +0200

    memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -10,4 +10,35 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void	*ft_memmove(void *s1, const void *s2, size_t n)
+{
+	int	i;
+
+	i = ((int)n) - 1;
+	if (!s1 && !s2)
+		return (NULL);
+	while (i >= 0)
+	{
+		((unsigned char *)s1)[i] = ((unsigned char *)s2)[i];
+		i--;
+	}
+	return (s1);
+}
+
+int	main(void)
+{
+	char str1[] = "123456789";
+	char str2[] = "abcdefghijklm";
+	char *s1 = str1;
+	char *s2 = str2;
+
+	printf("%s\n", memmove(s1 + 1, s2, 5));
+	printf("%s\n", str1);
+	printf("%s\n", ft_memmove(s1 + 1, s2, 5));
+	return (0);
+}
 
