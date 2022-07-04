65aeeab11ba3eb405d64b7635c0138a2c9cf3d2b
Xoanm <74424891+Xoanm@users.noreply.github.com>
2022-07-04 09:57:31 +0000

    strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -14,34 +14,28 @@
 
 int	ft_strncmp(const char *str1, const char *str2, size_t len)
 {
-	size_t i;
-	size_t j;
+size_t			i;
+	unsigned char	*s1;
+	unsigned char	*s2;
 
+	s1 = (unsigned char *)str1;
+	s2 = (unsigned char *)str2;
 	i = 0;
-	j = 0;
-	if (i == strlen(str2))
-		return(str2 - str1);
-	while (str1[i] && i < len)
+	while (len > 0)
 	{
-		j = 0;
-		while (str2[j] == str1[i + j] && (i + j) < len)
-		{
-			if (str2[j + 1] == str1[j + 1])
-			{
-				return (str2 - str1);
-			}
-			j++;
-		}
+		if (s1[i] != s2[i] || s1[i] == 0 || s2[i] == 0)
+			return (s1[i] - s2[i]);
 		i++;
+		len--;
 	}
-	return (str2 - str1);
-}
+	return (0);
+}/*
 int	main(void)
 {
-	char *s1 = "holaa";
-	char *s2 = "holas";
+	char *s1 = "hola";
+	char *s2 = "hol";
 	printf("Funcion original: %d\n", strncmp(s1, s2, 5));
 	printf("%d\n", ft_strncmp(s1, s2, 5));
 
 	return(0);
-}
+}*/
