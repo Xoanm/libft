50b9c67118fdbfa3e247f6906ce4fc2ecdbbad01
juan <juancarlos.morcillo@gmail.com>
2022-07-05 14:41:55 +0200

    calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -9,3 +9,28 @@
 /*   Updated: 2022/07/02 09:41:27 by jumorcil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+    char    *ptr;
+    int	    i;
+
+    i = 0;
+    ptr = (char *)malloc(nmemb * size);
+    if (!ptr)
+	return (0);
+    while (i < nmemb)
+    {
+	ptr[i] = 0;
+	i++;
+    }
+    return(ptr);
+}
+/*
+int main(void)
+{
+    printf("%p\n", ft_calloc(10, 2));
+    return (0);
+}*/
